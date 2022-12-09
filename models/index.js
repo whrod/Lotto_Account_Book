dd08ae6308b01d6cbb1cf1bd0d2474c8a0e7c5a8
whrod <whrodtube@gmail.com>
2022-12-10 01:32:59 +0900

    Fix : fix models/index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const Sequelize = require('sequelize');
+const User = require('./user');
+const SocialType = require('./social_type');
 const process = require('process');
 // const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
 let sequelize;
